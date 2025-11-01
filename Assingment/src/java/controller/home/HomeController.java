009a0c393e9b4a6a2a93efba9e7e750155cd2eac
soicodoc666 <baovh285@gmail.com>
2025-11-01 16:10:13 +0700

    update home
    update home

--- Assingment/src/java/controller/home/HomeController.java
+++ Assingment/src/java/controller/home/HomeController.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controller.home;
 
 import controller.iam.BaseRequiredAuthenticationController;
@@ -12,19 +8,21 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import model.iam.User;
 
-/**
- *
- * @author sonnt
- */
 @WebServlet(urlPatterns = "/home")
 public class HomeController extends BaseRequiredAuthenticationController {
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp, User user) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp, User user)
+            throws ServletException, IOException {
+        // Không có xử lý form
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp, User user) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp, User user)
+            throws ServletException, IOException {
+
+        // Gửi user sang JSP
+        req.setAttribute("user", user);
+        req.getRequestDispatcher("view/home.jsp").forward(req, resp);
     }
-    
 }
